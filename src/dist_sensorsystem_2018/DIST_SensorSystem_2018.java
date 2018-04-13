17e32e7b197300c60b2a7dd90d25ff1d89b18551
Snowspect <s155384@student.dtu.dk>
2018-04-13 12:42:19 +0200

    First Commit

---
+++ src/dist_sensorsystem_2018/DIST_SensorSystem_2018.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dist_sensorsystem_2018;
+
+/**
+ *
+ * @author tooth
+ */
+public class DIST_SensorSystem_2018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
