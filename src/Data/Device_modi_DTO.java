f1db1903ddaf4cc3eccfbcbdde734b999fcbf8e7
Snowspect <s155384@student.dtu.dk>
2018-04-17 14:31:52 +0200

    tmp

--- src/Data/Device_modi_DTO.java
+++ src/Data/Device_modi_DTO.java
@@ -8,6 +8,7 @@ package Data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -19,10 +20,11 @@ public class Device_modi_DTO {
 
     /**
      * Changes owner
+     *
      * @param owner
      * @return
      * @throws SQLException
-     * @throws ClassNotFoundException 
+     * @throws ClassNotFoundException
      */
     public static String Change_owner(String owner) throws SQLException, ClassNotFoundException {
         //try to connect to jdbc and create user
@@ -62,10 +64,11 @@ public class Device_modi_DTO {
 
     /**
      * changes lastActive Date
+     *
      * @param lastActive_Date
      * @return
      * @throws SQLException
-     * @throws ClassNotFoundException 
+     * @throws ClassNotFoundException
      */
     public static String Change_lastActive(String lastActive_Date) throws SQLException, ClassNotFoundException {
         //try to connect to jdbc and create user
@@ -105,10 +108,11 @@ public class Device_modi_DTO {
 
     /**
      * Changes device name
+     *
      * @param device_name
      * @return
      * @throws SQLException
-     * @throws ClassNotFoundException 
+     * @throws ClassNotFoundException
      */
     public static String Change_Name(String device_name) throws SQLException, ClassNotFoundException {
         //try to connect to jdbc and create user
@@ -145,4 +149,70 @@ public class Device_modi_DTO {
             return e.getMessage();
         }
     }
+
+    /**
+     * Pulls all devices related to one owner.
+     * @param owner
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    public static String Pull_device(String owner) throws SQLException, ClassNotFoundException {
+        //try to connect to jdbc and create user
+        try {
+            // create a mysql database connection
+            Class.forName(Conn.DRIVER); //Conn is our connection file
+            Connection conn = DriverManager.getConnection //Connection is a built in SQL class
+                    (
+                            Conn.DATABASE,
+                            Conn.USER,
+                            Conn.PASS
+                    );
+            // the mysql insert statement, adding a person into person table
+            String query = "SELECT * FROM device WHERE OWNER = ?";
+
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, owner);
+
+            ResultSet rs = preparedStmt.executeQuery();
+
+            String test = "";
+            while (rs.next()) {
+                test = rs.getString("ID_DEVICE");
+                test.concat(" : " + "NAME");
+                test.concat(" : " + "OWNER");
+                test.concat(" : " + "LASTACTIVE_DATE");
+                test.concat(" : " + "CREATED_DATE");
+            }
+
+            //close connection
+            conn.close();
+
+            //never displayed
+            System.out.println("Test variable print : " + test);
+            return test;
+        } catch (SQLException e) {
+            System.err.println("Got an sql exception!");
+            System.err.println(e.getMessage());
+            return e.getMessage();
+        } catch (Exception e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+            return e.getMessage();
+        }
+    }
+    
+    /**
+     * pulls all devices with id and name based on owner.
+     * @param owner
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    public static String Pull_Id_and_Name(String owner) throws SQLException, ClassNotFoundException
+    {
+        
+        return "";
+    }
+    
 }
