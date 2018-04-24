6948984f211c54b4cfdb0ec96f5960fece8d5d2e
Patrick Green Knudsen <tilgreenkundsen@gmail.com>
2018-04-24 15:42:24 +0200

    interface for rest
    Edited the interface for communcation with rest

--- src/SensorSystemServer/SensorSystemImplements.java
+++ src/SensorSystemServer/SensorSystemImplements.java
@@ -23,83 +23,82 @@ import javax.xml.ws.Service;
 @WebService(endpointInterface = "SensorSystemServer.SensorSystemInterface")
 public class SensorSystemImplements implements SensorSystemInterface {
 
-    private HashMap<Integer, User> activeUsers;
-    private HashMap<Integer, SensorSystemLogic> activeEnvironment;
-
-    public SensorSystemImplements() {
-        activeUsers = new HashMap<>();
-        
-    }
-    public void putUserInLogic(int user){
-        if(activeUsers.containsKey(user)) {
-            activeEnvironment.put(user, new SensorSystemLogic());
-        }
-    }
-    
-    public void setDeviceSensorData(ArrayList<Integer> data, int user, int deviceID, int sensorID) {
-        if (activeUsers.containsKey(user)) {
-            activeUsers.get(user).devices.get(deviceID).getSensor(sensorID).setData(data);
-        }
-    }
-
-    public void setDeviceName(int user, int deviceID, String name) {
-        if (activeUsers.containsKey(user)) {
-            activeUsers.get(user).devices.get(deviceID).setDeviceName(name);
-        }
-    }
-
-    public void setSensorName(int user, int deviceID, int sensorID, String name) {
-        if (activeUsers.containsKey(user)) {
-            activeUsers.get(user).devices.get(deviceID).getSensor(sensorID).setName(name);
-        }
-    }
-
-    public void addDevice(int user, String name) {
-        if (activeUsers.containsKey(user)) {
-            User u = activeUsers.get(user);
-
-            u.devices.add(new Device(u.devices.size(), name));
-        }
-    }
-
-    public void addSensor(int user, int deviceID, String name) {
-        if (activeUsers.containsKey(user)) {
-            User u = activeUsers.get(user);
-            u.devices.get(deviceID).addSensor(name);
-
-        }
-    }
-
-    public void deleteDevice(int user, int deviceID) {
-        if (activeUsers.containsKey(user)) {
-            activeUsers.get(user).devices.remove(deviceID);
-        }
-    }
-
-    public void deleteSensor(int user, int deviceID, int sensorID) {
-        if (activeUsers.containsKey(user)) {
-            activeUsers.get(user).devices.get(deviceID).removeSensor(sensorID);
-        }
-    }
-    
-    public int login(String user, String password) {
-        try {
-            System.out.println("Login: " + user);
-            URL url = new URL("http://javabog.dk:9901/brugeradmin?wsdl");
-            QName qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplService");
-            Service service = Service.create(url, qname);
-            Useradmin ba = service.getPort(Useradmin.class);
-
-            User b = ba.hentBruger(user, password);
-            String id_string = b.adgangskode + ":" + b.campusnetId + ":" + b.studeretning;
-            int id_code = id_string.hashCode();
-            activeUsers.put(id_code, b);
-            return id_code;
-
-        } catch (Exception ex) {
-            Logger.getLogger(SensorSystemImplements.class.getName()).log(Level.SEVERE, null, ex);
-            return 0;
-        }
-    }
+//    private HashMap<Integer, User> activeUsers;
+//
+//    public SensorSystemImplements() {
+//        activeUsers = new HashMap<>();
+//        
+//    }
+//    public void putUserInLogic(int user){
+//        if(activeUsers.containsKey(user)) {
+//            activeEnvironment.put(user, new SensorSystemLogic());
+//        }
+//    }
+//    
+//    public void setDeviceSensorData(ArrayList<Integer> data, int user, int deviceID, int sensorID) {
+//        if (activeUsers.containsKey(user)) {
+//            activeUsers.get(user).devices.get(deviceID).getSensor(sensorID).setData(data);
+//        }
+//    }
+//
+//    public void setDeviceName(int user, int deviceID, String name) {
+//        if (activeUsers.containsKey(user)) {
+//            activeUsers.get(user).devices.get(deviceID).setDeviceName(name);
+//        }
+//    }
+//
+//    public void setSensorName(int user, int deviceID, int sensorID, String name) {
+//        if (activeUsers.containsKey(user)) {
+//            activeUsers.get(user).devices.get(deviceID).getSensor(sensorID).setName(name);
+//        }
+//    }
+//
+//    public void addDevice(int user, String name) {
+//        if (activeUsers.containsKey(user)) {
+//            User u = activeUsers.get(user);
+//
+//            u.devices.add(new Device(u.devices.size(), name));
+//        }
+//    }
+//
+//    public void addSensor(int user, int deviceID, String name) {
+//        if (activeUsers.containsKey(user)) {
+//            User u = activeUsers.get(user);
+//            u.devices.get(deviceID).addSensor(name);
+//
+//        }
+//    }
+//
+//    public void deleteDevice(int user, int deviceID) {
+//        if (activeUsers.containsKey(user)) {
+//            activeUsers.get(user).devices.remove(deviceID);
+//        }
+//    }
+//
+//    public void deleteSensor(int user, int deviceID, int sensorID) {
+//        if (activeUsers.containsKey(user)) {
+//            activeUsers.get(user).devices.get(deviceID).removeSensor(sensorID);
+//        }
+//    }
+//    
+//    public int login(String user, String password) {
+//        try {
+//            System.out.println("Login: " + user);
+//            URL url = new URL("http://javabog.dk:9901/brugeradmin?wsdl");
+//            QName qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplService");
+//            Service service = Service.create(url, qname);
+//            Useradmin ba = service.getPort(Useradmin.class);
+//
+//            User b = ba.hentBruger(user, password);
+//            String id_string = b.adgangskode + ":" + b.campusnetId + ":" + b.studeretning;
+//            int id_code = id_string.hashCode();
+//            activeUsers.put(id_code, b);
+//            return id_code;
+//
+//        } catch (Exception ex) {
+//            Logger.getLogger(SensorSystemImplements.class.getName()).log(Level.SEVERE, null, ex);
+//            return 0;
+//        }
+//    }
 
 }
--- src/SensorSystemServer/SensorSystemInterface.java
+++ src/SensorSystemServer/SensorSystemInterface.java
@@ -5,6 +5,10 @@
  */
 package SensorSystemServer;
 
+import UserInterface.Device;
+import UserInterface.Sensor;
+import java.util.ArrayList;
+import java.util.Date;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
@@ -17,4 +21,22 @@ import javax.jws.WebService;
 public interface SensorSystemInterface {
     
     @WebMethod public int login(String user, String password);
+    
+    @WebMethod public void set_Sensor_Info(int sensor_id, int device_id_ref, int type, int pin, String name);
+    @WebMethod public void set_Device_Info(int device_id, String owner, String name);
+    
+    @WebMethod public Sensor get_Sensor_Info(int sensor_id);
+    @WebMethod public Device get_Device_Info(int device_id);
+    
+    @WebMethod public void delete_Sensor(int sensor_id);
+    @WebMethod public void delete_Device(int device_id);
+    
+    @WebMethod public ArrayList<Device> get_Devices(int owner);
+    @WebMethod public void delete_Devices(int owner);
+    
+    @WebMethod public ArrayList<String> get_All_Sensor_Data(int sensor_id);
+    @WebMethod public ArrayList<String> get_All_Sensor_Data_Within_Dates(int sensor_id, Date older, Date newer);
+    
+
+    
 }
