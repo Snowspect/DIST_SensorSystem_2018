19c4b2e84ce8ddc12e2ec9f6c6c8eb2a5321c2e7
Patrick Green Knudsen <tilgreenkundsen@gmail.com>
2018-04-27 17:31:05 +0200

    kkk

---
+++ src/SensorSystemServer/client.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SensorSystemServer;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+/**
+ *
+ * @author Bruger
+ */
+public class client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] arg) throws Exception
+    {
+
+        Scanner reader = new Scanner(System.in);
+        URL url = new URL("http://159.89.134.40:9901/SensorSystemService?wsdl");
+        QName qname = new QName("http://SensorSystemServer/", "SensorSystemImplementsService");
+        Service service = Service.create(url, qname);
+        SensorSystemInterface s = service.getPort(SensorSystemInterface.class);
+
+
+        int id = login(s, reader);
+        
+        System.out.print(id);
+        
+        ArrayList<ArrayList<Integer>> ids = s.get_ids(id);
+        
+        if(ids.isEmpty())
+            return;
+        System.out.print(ids);
+    }
+    
+    public static int login(SensorSystemInterface server, Scanner s) {
+        int id = 0;
+        while(id == 0) {
+            
+            System.out.println("Input username:");
+            String username = s.nextLine();
+            System.out.println("Input password:");
+            String password = s.nextLine();
+            
+            try {
+                System.out.println("HEJ");
+                id = server.login(username, password);
+            } catch (Exception e) {
+                System.out.println("Login failed..");
+            }
+        }
+        return id;
+    }    
+        
+    
+}
