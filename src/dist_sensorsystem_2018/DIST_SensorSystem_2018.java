66fcd493076f49204da4cd98b9ed761fef995d8c
Snowspect <s155384@student.dtu.dk>
2018-04-27 16:33:36 +0200

    Attempting to access Device id's.
    Need to add login to DIST_SensorSystem_2018 first

--- src/dist_sensorsystem_2018/DIST_SensorSystem_2018.java
+++ src/dist_sensorsystem_2018/DIST_SensorSystem_2018.java
@@ -7,8 +7,10 @@ package dist_sensorsystem_2018;
 
 import static Data.Device_DTO.device_CreateDevice;
 import static Data.Sensor_DTO.sensor_CreateSensor;
+import SensorSystemServer.SensorSystemImplements;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -22,12 +24,29 @@ public class DIST_SensorSystem_2018 {
      */
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // TODO code application logic here
+        
+        
+        
         try{
             System.out.println("hellooooo1");
                             Date dt = new Date();
                 Timestamp dx = new Timestamp(dt.getTime());
-            //device_CreateDevice(2,"test1","Dan", dx, dx);
-            sensor_CreateSensor("sensor1", 1, "DIGITAL", "36", dx, dx, "4");
+            //device_CreateDevice(2,"3","42", dx, dx);
+            //sensor_CreateSensor("sensor1", 3, "DIGITAL", "36", dx, dx, "4");
+            SensorSystemImplements d = new SensorSystemImplements();
+            ArrayList<ArrayList<Integer>> _ids = d.get_ids(42);
+            if(_ids == null)
+            {
+                System.out.println("Her er ingen information");
+            }
+            else{
+                System.out.println("");
+                for (ArrayList<Integer> _id : _ids) {
+                    for (Integer integer : _id) {
+                        System.out.println(_id);
+                    }
+                }
+            }
         }
         catch(Exception e)
         {
