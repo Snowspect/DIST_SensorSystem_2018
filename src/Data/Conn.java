0581f4cdf3e0a6d611465682cae8cffac4a7e213
Snowspect <s155384@student.dtu.dk>
2018-05-04 11:26:24 +0200

    bug fix

--- src/Data/Conn.java
+++ src/Data/Conn.java
@@ -5,11 +5,6 @@
  */
 package Data;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 /**
  *
  * @author tooth
@@ -17,8 +12,4 @@ import java.sql.SQLException;
 public class Conn 
 {
     static final String CONNECTION_STRING = "jdbc:mariadb://165.227.232.158:3306/sensorsystem?user=Dist&password=*54B7B2B2CAA56DB962B1BD8C9CE7EEE49BB31B2B";
-    static final String USER = "Dist"; //dan    
-    static final String PASS = "1234"; //itElektronik2019!     
-    static final String DRIVER = "org.mariadb.jdbc.Driver"; //com.mysql.jdbc.Driver //org.mariadb.jdbc.Driver
-    static final String DATABASE = "jdbc:mariadb://159.89.134.40:22/sensorsystem"; //jdbc:mysql://128.76.255.24:25800/question    //jdbc:mysql://localhost:3306/question2´    
 }
